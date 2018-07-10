1381234ed4fd4377157d268fd38bce4640d31502
martinmzhou <1181053849@qq.com>
2018-07-10 21:09:28 +0800

    first commmit

---
+++ backend/src/main/java/com/adshow/web/SpringBootVuejsApplication.java
@@ -0,0 +1,14 @@
+package com.adshow.web;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.adshow.web.dao")
+public class SpringBootVuejsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootVuejsApplication.class, args);
+	}
+}
