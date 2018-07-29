a64f994377af317ba2005d000b4cb0597d5065e1
yuan3140 <562720180@qq.com>
2018-07-29 15:56:00 +0800

    upload img

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,9 +6,12 @@ import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import 'normalize.css'
+import './style/index.less'
+import CONFIG from  './CONFIG.vue'
 
 Vue.config.productionTip = false
 Vue.use(ElementUI, {size: 'small'})
+Vue.prototype.GLOBAL = CONFIG
 
 /* eslint-disable no-new */
 new Vue({
