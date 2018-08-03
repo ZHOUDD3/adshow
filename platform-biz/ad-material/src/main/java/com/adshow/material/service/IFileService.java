356dc4a8a52cd3fb09e1630a2f43ffd2330a1ab0
nemozhao <zhaoxianbin.dx@163.com>
2018-08-04 00:39:16 +0800

    广告部分基础CRUD

--- platform-biz/ad-material/src/main/java/com/adshow/material/service/IFileService.java
+++
@@ -1,9 +0,0 @@
-package com.adshow.material.service;
-
-import com.adshow.mapper.entity.VideoManage;
-import com.baomidou.mybatisplus.service.IService;
-
-public interface IFileService extends IService  {
-
-
-}
